5f87008af49432e4e9883881a827e33d1a8eea18
Kamil <kamilm2003@gmail.com>
2023-02-27 22:12:23 +0100

    as

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const request = new Request("");
+  // const requestHeaders = new Headers(Request.headers);
+
+  // Add new request headers
+  request.headers.append("Access-Control-Allow-Origin", "*");
+  console.log(request);
+  return <Component {...pageProps} />;
 }
