daf1020651e75d84bc51b7e5baca75b27845cc87
Kamil <kamilm2003@gmail.com>
2023-03-04 11:48:03 +0100

    PH chart - don't display lines when protein isn't selected

--- frontend/components/PHChart.tsx
+++ frontend/components/PHChart.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 
 interface IProps {
-  ph?: number; // 0-14
-  index?: number; // 0-14
+  ph?: number | null; // 0-14
+  index?: number | null; // 0-14
 }
 
-export default function PHChart({ ph = 3, index = 3 }: IProps) {
-  // TODO dont display lines when no protein is selected
-  const roundedPh = Math.round(ph * 100) / 100;
-  const roundedIndex = Math.round(index * 100) / 100;
-  const phPosition = (ph / 14) * 100; // in percentage
-  const indexPosition = (index / 14) * 100; // in percentage
+export default function PHChart({ ph, index }: IProps) {
+  let roundedPh = 0;
+  let roundedIndex = 0;
+  let phPosition = 0;
+  let indexPosition = 0;
+
+  if (ph) {
+    roundedPh = Math.round(ph * 100) / 100;
+    phPosition = (ph / 14) * 100; // in percentage
+  }
+  if (index) {
+    roundedIndex = Math.round(index * 100) / 100;
+    indexPosition = (index / 14) * 100; // in percentage
+  }
 
   console.log(ph, roundedPh);
   console.log(index, roundedIndex);
@@ -24,16 +32,19 @@ export default function PHChart({ ph = 3, index = 3 }: IProps) {
       <div className="relative w-full h-full">
         {/* Fill */}
         <div className="relative w-32 h-full bg-gradient-to-t from-red-600 via-yellow-500 to-blue-600 rounded-lg">
-          {/* TODO dwie krechy */}
           {/* PH line */}
           <div
             style={{ top: `${phPosition}%` }}
-            className="w-full h-1 bg-green-500 left-0 absolute transform -translate-y-1/2 transition-all duration-700"
+            className={`w-full h-1 bg-green-500 left-0 absolute transform -translate-y-1/2 transition-all duration-700 ${
+              ph ? "opacity-100" : "opacity-0"
+            }`}
           ></div>
           {/* Index line */}
           <div
             style={{ top: `${indexPosition}%` }}
-            className="w-full h-1 bg-purple-500 left-0 absolute transform -translate-y-1/2 transition-all duration-1000"
+            className={`w-full h-1 bg-purple-500 left-0 absolute transform -translate-y-1/2 transition-all duration-1000 ${
+              ph ? "opacity-100" : "opacity-0"
+            }`}
           ></div>
         </div>
         {/* PH scale 'numbers' */}
@@ -42,13 +53,17 @@ export default function PHChart({ ph = 3, index = 3 }: IProps) {
         </span>
         <span
           style={{ top: `${phPosition}%` }}
-          className="text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-700"
+          className={`text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-700 ${
+            ph ? "opacity-100" : "opacity-0"
+          }`}
         >
           {roundedPh}
         </span>
         <span
           style={{ top: `${indexPosition}%` }}
-          className="text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-1000"
+          className={`text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-1000 ${
+            ph ? "opacity-100" : "opacity-0"
+          }`}
         >
           {roundedIndex}
         </span>
