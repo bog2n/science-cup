135708dbe5e70cd9d213687737c30aab15be989b
Kamil <kamilm2003@gmail.com>
2023-03-04 18:29:19 +0100

    Ph chart fix - values that are too close to each other

--- frontend/components/PHChart.tsx
+++ frontend/components/PHChart.tsx
@@ -10,6 +10,7 @@ export default function PHChart({ ph, index }: IProps) {
   let roundedIndex = 0;
   let phPosition = 0;
   let indexPosition = 0;
+  let offset = false;
 
   if (ph) {
     roundedPh = Math.round(ph * 100) / 100;
@@ -18,6 +19,13 @@ export default function PHChart({ ph, index }: IProps) {
   if (index) {
     roundedIndex = Math.round(index * 100) / 100;
     indexPosition = (index / 14) * 100; // in percentage
+  }
+
+  // Add offset if values are too close.
+  if (Math.abs(roundedPh - roundedIndex) < 1.5) {
+    offset = true;
+  } else {
+    offset = false;
   }
 
   return (
@@ -50,16 +58,16 @@ export default function PHChart({ ph, index }: IProps) {
           14
         </span>
         <span
-          style={{ bottom: `${phPosition}%` }}
-          className={`text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-700 ${
+          style={{ bottom: `${offset ? phPosition - 4 : phPosition}%` }}
+          className={`text-xl font-bold right-full mr-2 absolute transform translate-y-[35%] transition-all duration-700 ${
             ph ? "opacity-100" : "opacity-0"
           }`}
         >
           {roundedPh}
         </span>
         <span
-          style={{ bottom: `${indexPosition}%` }}
-          className={`text-xl font-bold right-full mr-2 absolute transform -translate-y-1/2 transition-all duration-1000 ${
+          style={{ bottom: `${offset ? indexPosition + 4 : indexPosition}%` }}
+          className={`text-xl font-bold right-full mr-2 absolute transform translate-y-[35%] transition-all duration-1000 ${
             ph ? "opacity-100" : "opacity-0"
           }`}
         >
